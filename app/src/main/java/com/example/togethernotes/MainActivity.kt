a711484fee3cf7ee6b1935c8e2f55053c241522a
Aimadpro <aaisad2324@politecnics.barcelona>
2025-02-05 19:45:57 +0100

    empiezo el layout del login

--- app/src/main/java/com/example/togethernotes/MainActivity.kt
+++ app/src/main/java/com/example/togethernotes/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var hola = 6
+
     }
 }
