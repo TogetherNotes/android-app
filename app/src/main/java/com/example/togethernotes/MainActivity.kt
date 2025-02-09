aceea2f4883973eb2c3d7ed53ae2f327bee01bff
Aimadpro <aaisad2324@politecnics.barcelona>
2025-02-09 12:58:39 +0100

    empiezo el layout del login

--- app/src/main/java/com/example/togethernotes/MainActivity.kt
+++ app/src/main/java/com/example/togethernotes/MainActivity.kt
@@ -1,14 +1,72 @@
 package com.example.togethernotes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navegation : BottomNavigationView
+    private val mOnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener {
+        item ->
+
+        when(item.itemId) {
+            R.id.itemFragment1 -> {
+                supportFragmentManager.commit {
+                    replace<MatchFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                        addToBackStack("replacment")
+                    }
+                    return@OnNavigationItemSelectedListener true
+                }
+
+            }
+        when(item.itemId) {
+            R.id.itemFragment2 -> {
+                supportFragmentManager.commit {
+                    replace<ChatFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                        addToBackStack("replacment")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        when(item.itemId) {
+            R.id.itemFragment3 -> {
+                supportFragmentManager.commit {
+                    replace<calendarFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacment")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        when(item.itemId) {
+            R.id.itemFragment4 -> {
+                supportFragmentManager.commit {
+                    replace<AccountFragment>(R.id.fragmentContainerView)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacment")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.show_menu_activity)
+        navegation = findViewById(R.id.navMenu)
+        navegation.setOnNavigationItemSelectedListener(mOnNavMenu)
 
+        supportFragmentManager.commit {
+            replace<MatchFragment>(R.id.fragmentContainerView)
+            setReorderingAllowed(true)
+            addToBackStack("replacment")
+        }
     }
 }
