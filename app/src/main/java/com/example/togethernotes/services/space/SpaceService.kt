4fce540e56fffae623057c45c2ad2588d9b347cc
Aimadpro <aaisad2324@politecnics.barcelona>
2025-04-02 20:13:57 +0200

    pre-fix

---
+++ app/src/main/java/com/example/togethernotes/services/space/SpaceService.kt
@@ -0,0 +1,27 @@
+package com.example.togethernotes.services.space
+
+import com.example.togethernotes.models.Space
+import retrofit2.Response
+import retrofit2.http.*
+
+interface SpaceService {
+
+    // GET: api/spaces
+    @GET("api/spaces")
+    suspend fun getAllSpaces(): Response<List<Space>>
+
+    // GET: api/spaces/{id}
+    @GET("api/spaces/{id}")
+    suspend fun getSpaceById(@Path("id") id: Int): Response<Space>
+
+    // PUT: api/spaces/{id}
+    @PUT("api/spaces/{id}")
+    suspend fun updateSpace(
+        @Path("id") id: Int,
+        @Body space: Space
+                           ): Response<Void>
+
+    // POST: api/spaces/register
+    @POST("api/spaces/register")
+    suspend fun registerSpace(@Body request: Space): Response<Map<String, Any>>
+}
